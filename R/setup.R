b20dd3b992b0926b016c0a219b73b154fc337679
Vegard Lysne <Vegard.Lysne@fhi.no>
2022-09-27 07:32:58 +0200

    moved package loading to setup.R

--- R/setup.R
+++ R/setup.R
@@ -1,2 +1,6 @@
+# Load packages
 source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 kh_load(data.table, dplyr, rlang, tidyr, stringr, purrr)
+
+# Load functions for quality control part 1
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step1.R")
