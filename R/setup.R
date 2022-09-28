5369bd68c88ba82b857960ab73074d10ab991ade
Vegard Lysne <Vegard.Lysne@fhi.no>
2022-09-28 11:35:11 +0200

    bugfix

--- R/setup.R
+++ R/setup.R
@@ -1,6 +1,6 @@
 # Load packages 
-source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 library(stats)
+source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 kh_load(dplyr, tidyr, stringr, purrr, data.table)
 
 # Remove redundant functions
