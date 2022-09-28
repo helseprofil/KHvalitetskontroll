b7b91d4253f4f40f0d9ac234c12f7b4fa848480f
Vegard Lysne <Vegard.Lysne@fhi.no>
2022-09-28 09:38:56 +0200

    renv init

--- R/setup.R
+++ R/setup.R
@@ -1,3 +1,8 @@
-# Load packages
+# Load packages 
 source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 kh_load(dplyr, tidyr, stringr, purrr, data.table)
+
+# Remove redundant functions
+rm(list = ls()[grepl("^kh_", ls())])
+rm(list = ls()[grepl("^pkg_", ls())])
+rm(list = ls()[grepl("^git_", ls())])
