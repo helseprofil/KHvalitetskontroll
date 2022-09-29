ea89b72a52f0d83de676dabafc735d3b237c9ea8
Vegard Lysne <Vegard.Lysne@fhi.no>
2022-09-29 09:34:39 +0200

    slett testfil

--- R/setup.R
+++ R/setup.R
@@ -1,5 +1,7 @@
-# Load packages 
+# Switch to main branch and pull any changes (add new files etc)
 gert::git_pull()
+
+# Load packages 
 source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 kh_load(dplyr, tidyr, stringr, purrr, data.table)
 
