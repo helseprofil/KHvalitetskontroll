51359535771a6103d2192e9983558616faa2b8c5
Vegard Lysne <Vegard.Lysne@fhi.no>
2022-09-29 10:26:24 +0200

    edit setup

--- R/setup.R
+++ R/setup.R
@@ -8,11 +8,13 @@ cat("\nAlle filer oppdatert\n")
 source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
 kh_load(dplyr, tidyr, stringr, purrr, data.table)
 
+# Source internal scripts
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/misc.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step1.R")
+
 # Remove redundant functions
 rm(list = ls()[grepl("^kh_", ls())])
 rm(list = ls()[grepl("^pkg_", ls())])
 rm(list = ls()[grepl("^is_not_", ls())])
 
-# Source internal scripts
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/misc.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step1.R")
+
