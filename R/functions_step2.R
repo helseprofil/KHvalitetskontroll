c15a4d782a3d6cf210ceb2959b8176a02c512c8d
Vegard Lysne <vegard.lysne@fhi.no>
2022-10-10 14:03:34 +0200

    FlagKubes can create filedumps

--- R/functions_step2.R
+++ R/functions_step2.R
@@ -1,4 +1,4 @@
-#' flag_new
+#' FlagNew
 #' 
 #' Initiate flagged version of dfnew (dfnew_flag), saved to global environment
 #' Identifies common and new dimensions compared to old KUBE
@@ -14,22 +14,22 @@
 #' @export
 #'
 #' @examples
-FlagNew <- function(data1 = dfnew, 
-                    data2 = dfold, 
-                    dims = DIMENSIONS){
+.FlagNew <- function(data1, 
+                    data2, 
+                    dims){
   
   # Identify dimension columns present in new and old KUBE
-  # Separate into new and same dimensions for flagging
+  # Separate into common and new dimensions for flagging
   dimnew <- names(data1[, names(data1) %in% dims, with = F])
   dimold <- names(data2[, names(data2) %in% dims, with = F])
   commondims <- dimnew[dimnew %in% dimold]
   newdims <- dimnew[!dimnew %in% dimold]
   
-  msgcommon <- case_when(length(commondims) == 0 ~ "No common dimensions found",
-                      TRUE ~ paste0("Common columns found: ", str_c(commondims, collapse = ", ")))
+  msgcommon <- case_when(length(commondims) == 0 ~ "\n-No common dimensions found",
+                      TRUE ~ paste0("\n-Common columns found: ", str_c(commondims, collapse = ", ")))
   
-  msgnew <- case_when(length(newdims) == 0 ~ "\nNo new dimensions.",
-                      TRUE ~ paste0("\nNew dimensions found: ", str_c(newdims, collapse = ", ")))
+  msgnew <- case_when(length(newdims) == 0 ~ "\n-No new dimensions.",
+                      TRUE ~ paste0("\n-New dimensions found: ", str_c(newdims, collapse = ", ")))
   
   cat(msgcommon)
   cat(msgnew)
@@ -39,26 +39,114 @@ FlagNew <- function(data1 = dfnew,
   dfnew_flag <<- copy(data1)[, newrow := 0L]
   setkeyv(dfnew_flag, c(commondims, newdims))
   
-  # For same dimensions, flag all rows with new levels 
+  # For common dimensions, flag all rows with new levels 
   # Loops over common dimensions. Flags previously unflagged rows for new levels 
   walk(commondims, 
        ~dfnew_flag[!dfnew_flag[[.x]] %in% unique(data2[[.x]]) & newrow == 0,
                    newrow := 1L])
   
-  cat("\nFor common dimensions, all rows with new levels are flagged")
+  cat("\n-For common dimensions, flagged all rows with new levels")
   
   # For new dimensions, flag any rows != 0 (i.e. not total numbers)
   if(length(newdims) != 0) {
     walk(newdims,
          ~dfnew_flag[dfnew_flag[[.x]] != 0 & newrow == 0,
                      newrow := 1L])
-    cat("\nFor new dimensions, all rows not representing total numbers are flagged ")
+    cat("\n-For new dimensions, flagged all rows not representing total numbers")
   } 
+  
+  cat("\n-Flagged version of new KUBE created: dfnew_flag")
 }
 
+#' FlagOld
+#' 
+#' Initiate flagged version of dfold (dfold_flag), saved to global environment
+#' Identifies common and expired dimensions compared to new KUBE
+#' For common dimensions, all expired rows are flagged (sets exprow = 1)
+#' For expired dimensions, all rows != 0 (total numbers) are flagged as expired
+#' 
+#' @param data1 new KUBE, defaults to dfnew
+#' @param data2 old KUBE, defaults to dfold
+#' @param dims character vector of dimensions columns, defaults to DIMENSIONS set in INPUT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.FlagOld <- function(data1,
+                    data2,
+                    dims){
+  
+  # Identify dimension columns present in old and new KUBE
+  # Separate into common and expired dimensions for flagging
+  dimnew <- names(data1[, names(data1) %in% dims, with = F])
+  dimold <- names(data2[, names(data2) %in% dims, with = F])
+  commondims <- dimold[dimold %in% dimnew]
+  expdims <- dimold[!dimold %in% dimnew]
+  
+  msgcommon <- case_when(length(commondims) == 0 ~ "\n-No common dimensions found",
+                         TRUE ~ paste0("\n-Common columns found: ", str_c(commondims, collapse = ", ")))
+  
+  msgexp <- case_when(length(expdims) == 0 ~ "\n-No expired dimensions.",
+                      TRUE ~ paste0("\n-Expired dimensions found: ", str_c(expdims, collapse = ", ")))
+  
+  cat(msgcommon)
+  cat(msgexp)
+  
+  # Initiate flagged version of old KUBE (sets newrow = 0), saves to global env
+  # Sorts KUBE according to common and new dims
+  dfold_flag <<- copy(data2)[, exprow := 0L]
+  setkeyv(dfold_flag, c(commondims, expdims))
+  
+  # For common dimensions, flag all rows with expired levels 
+  # Loops over common dimensions. Flags previously unflagged rows for expired levels 
+  walk(commondims, 
+       ~dfold_flag[!dfold_flag[[.x]] %in% unique(data1[[.x]]) & exprow == 0,
+                   exprow := 1L])
+  
+  cat("\n-For common dimensions, flagged all rows with expired levels")
+  
+  # For expired dimensions, flag any rows != 0 (i.e. not total numbers)
+  if(length(expdims) != 0) {
+    walk(expdims,
+         ~dfold_flag[dfold_flag[[.x]] != 0 & exprow == 0,
+                     exprow := 1L])
+    cat("\n-For expired dimensions, flagged all rows not representing total numbers")
+  } 
+  
+  cat("\n-Flagged version of old KUBE created: dfold_flag")
+}
 
-###########
-# -----
-###########
+FlagKubes <- function(data1 = dfnew,
+                      data2 = dfold,
+                      dims = DIMENSIONS,
+                      dumps = DUMPS,
+                      dumpname = DUMPname){
+  
+  cat("STARTS flagging new kube")
+  .FlagNew(data1 = data1,
+          data2 = data2,
+          dims = dims)
+  cat("\n\nSTARTS flagging old kube")
+  .FlagOld(data1 = data1,
+          data2 = data2,
+          dims = dims)
+  cat("\n\nCOMPLETED flagging\n")
+  
+  if("dfnew_flag" %in% dumps){
+    fwrite(dfnew_flag, 
+           file = paste0(dumpname, "_flagged.csv"),
+           sep = ";")
+    cat("\nFILEDUMP: Flagged new KUBE")
+  }
+  
+  if("dfold_flag" %in% dumps){
+    fwrite(dfold_flag, 
+           file = paste0(dumpname, "_old_flagged.csv"),
+           sep = ";")
+    cat("\nFILEDUMP: Flagged old KUBE")
+  }
+
+}
 
 
