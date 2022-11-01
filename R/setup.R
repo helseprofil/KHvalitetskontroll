fda8320631fc118e50c0cf4960fb3cf093321199
Vegard Lysne <vegard.lysne@fhi.no>
2022-11-01 15:09:18 +0100

    fix color

--- R/setup.R
+++ R/setup.R
@@ -34,8 +34,3 @@ theme_set(theme_bw())
 theme_update(legend.position = "bottom", 
              panel.grid.minor = element_blank(),
              text = element_text(color = "black"))
-
-# options(ggplot2.continuous.colour = "viridis")
-# options(ggplot2.continuous.fill = "viridis")
-# options(ggplot2.discrete.colour = "viridis")
-# options(ggplot2.discrete.fill = "viridis")
