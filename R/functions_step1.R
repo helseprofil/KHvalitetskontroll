1df3cde9c6e106fe9f140a047e31d61fe5bfc11f
Vegard Lysne <vegard.lysne@fhi.no>
2022-12-29 12:22:39 +0100

    CompareFylkeLand to data.table

--- R/functions_step1.R
+++ R/functions_step1.R
@@ -246,37 +246,47 @@ CheckPrikk <- function(data1 = dfnew,
 #' @export
 #'
 #' @examples
-CompareFylkeLand <- function(data1 = dfnew, groupdim = GROUPdims, compare = COMPAREval){
+CompareFylkeLand <- function(data = dfnew, groupdim = GROUPdims, compare = COMPAREval){
   
-  data <- data1 %>% 
-    dplyr::filter(GEO < 100) %>% 
-    dplyr::filter(!(GEO %in% 81:84)) %>% # Remove HELSEREGION
-    mutate(geolevel = case_when(GEO == 0 ~ "Land",
-                                GEO < 100 ~ "Fylke")) 
-  
-  output <- data %>% 
-    group_by(across(c(geolevel, all_of(groupdim)))) %>% 
-    summarise(sum = sum(.data[[compare]], na.rm = T), .groups = "drop") %>% 
-    pivot_wider(names_from = geolevel, 
-                values_from = sum) %>% 
-    mutate(Absolute = Land-Fylke,
-           Relative = Land/Fylke) %>% 
-    arrange(desc(Relative)) %>% 
-    mutate(across(c(Land, Fylke, Absolute), ~round(.x, 0)),
-           across(Relative, ~case_when(Relative == Inf ~ NA_real_,
-                                       TRUE ~ round(Relative, 3)))) %>% 
-    select(all_of(groupdim), Land, Fylke, Absolute, Relative)
+  # Create subset 
+  data <- copy(data)[GEO<100 & !GEO %in% 81:84]
+  data[, geolevel := "Fylke"]
+  data[GEO == 0, geolevel := "Land"]
   
   cat("GEOcodes included: ", str_c(unique(data$GEO), collapse = ", "), "\n")
   
-  if(nrow(output %>%
-          dplyr::filter(Relative < 1)) == 0) {
+  # Sum compare value per strata of geolevel and grouping dims
+  data <- data[, .("sum" = sum(get(compare), na.rm = T)), keyby = c("geolevel", groupdim)]
+  
+  # Format output
+  data <- dcast(data, ... ~ geolevel, value.var = "sum")
+  
+  # Estimate absolute and relative difference, format digits
+  data[, `:=` (Absolute = Land - Fylke,
+               Relative = round(Land/Fylke, 3))]
+  format <- c("Land", "Fylke", "Absolute")
+  data[, (format) := lapply(.SD, round, 0), .SDcols = format]
+  
+  if(nrow(data[Relative < 1]) == 0) {
     cat("\nLAND is always larger than FYLKE")
   } else {
     cat("\nIn some rows, FYLKE is larger than LAND.\n See rows with Absolute < 1")
   }
    
-  datatable(output, rownames = F)
+  # Convert groupdim to factor and set column order
+  data[, (groupdim) := lapply(.SD, as.factor), .SDcols = c(groupdim)]
+  setcolorder(data, c(groupdim, "Land", "Fylke", "Absolute", "Relative"))
+  
+  DT::datatable(data[order(-Relative)], 
+                filter = "top",
+                rownames = F,
+                options = list(
+                  columnDefs = list(list(targets = c("Land", "Fylke", "Absolute", "Relative"),
+                                         searchable = FALSE)),
+                  # Show length menu, table, pagination, and information
+                  dom = 'ltpi', 
+                  scrollX = TRUE)
+  )
 }
 
 #' CompareKommuneFylke
