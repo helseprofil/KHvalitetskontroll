27ffa0db18029603fe1ad08e5b82bdc2d71d183e
Vegard Lysne <vegard.lysne@fhi.no>
2023-05-02 16:09:27 +0200

    change file paths to main branch

--- R/setup.R
+++ R/setup.R
@@ -28,12 +28,12 @@ conflicted::conflict_prefer("last", "data.table", quiet = T)
 conflicted::conflict_prefer("transpose", "data.table", quiet = T)
 
 # Load internal functions
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/misc.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/functions_step1.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/functions_step2.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/functions_friskvik.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/functions_interactive.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/globals.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/misc.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step1.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step2.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_friskvik.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_interactive.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/globals.R")
 
 # Source welcome messages
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/dev/R/welcome.R")
+source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/welcome.R")
