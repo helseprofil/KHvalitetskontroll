4358ca1d8c0e5ab098c48b879c64bdc5f2627fb4
Vegard Lysne <vegard.lysne@fhi.no>
2023-05-05 13:18:51 +0200

    move packages and functions to separate file

--- R/setup.R
+++ R/setup.R
@@ -2,35 +2,6 @@
 source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/updateproject.R")
 
 # Load packages
-library(conflicted)
-library(RODBC)
-library(dplyr)
-library(forcats)
-library(stringr)
-library(purrr)
-library(DT)
-library(ggplot2)
-library(ggh4x)
-library(norgeo)
-library(collapse)
-library(data.table)
-
-# Solve conflicts
-conflicted::conflict_prefer("filter", "dplyr", quiet = T)
-conflicted::conflict_prefer("lag", "dplyr", quiet = T)
-conflicted::conflict_prefer("D", "collapse", quiet = T)
-conflicted::conflict_prefer("between", "data.table", quiet = T)
-conflicted::conflict_prefer("first", "data.table", quiet = T)
-conflicted::conflict_prefer("last", "data.table", quiet = T)
-conflicted::conflict_prefer("transpose", "data.table", quiet = T)
-
-# Load internal functions
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/misc.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step1.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_step2.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_friskvik.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/functions_interactive.R")
-source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/globals.R")
 
 # Source welcome messages
 source("https://raw.githubusercontent.com/helseprofil/KHvalitetskontroll/main/R/welcome.R")
