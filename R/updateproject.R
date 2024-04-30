2b4672b8800dc4d2251ddf7cbc537fceade239f7
Vegard Lysne <vegard.lysne@fhi.no>
2024-04-30 11:53:50 +0200

    updateproject only updating if .lastupdate is > local updates

--- R/updateproject.R
+++ R/updateproject.R
@@ -1,32 +1,39 @@
-# Change according to when the user files were last updated
+# Change according to when the user files or packages were last updated
 .lastupdate <- "30.04.2024"
 
-.updateproject <- function(lu){
+# Find latest local update date
+.localupdate <- grep(".lastupdate <-", readLines("R/updateproject.R", n = 3), value = T)
+.localupdate <- sub(".*(\\d{2}.\\d{2}.\\d{4}).*", "\\1", .localupdate)
+
+.updateproject <- function(lu, loc){
   
   # Check if main branch is active, if not return and let the user keep on dev work. 
   b <- system("git branch --show-current", intern = TRUE)
+  
   if(b != "main"){
     message("\nYou are on the branch '", b, "'.\nKeep on the good dev work or switch to the main branch `system(git checkout main)` to continue with quality control!")
     return(invisible(NULL))
-  } else {
-    message("\nYou are on the main branch, ready for some good old quality control, enjoy!")
-  }
+  } 
   
-  # Update all files if on master branch
+  message("\nYou are on the main branch")
   
-  if(b == "main"){
+  # Update all files if on master branch and updates available
+  if(lu != loc){
     choice <- menu(choices = c("Yes", "No"),
-                   title = paste0("\nUpdate package version and user files (recommended)?",
-                                  "\nLast updated on: ", lu))
+                   title = paste0("\nPackages or USER files updates available!!",
+                                  "\nLast updated on: ", lu,
+                                  "\nUpdate files (recommended)?"))
     
     if(choice == 1){
+      message("\nFetching updates...")
       invisible(system("git fetch origin main"))
       invisible(system("git reset --hard origin/main"))
       invisible(system("git pull"))
     } else {
-      message("\nSkipping update, your packages and user files might be outdated")
+      message("\nSkipping updates, your packages and USER files might be outdated")
     }
   }
 }
 
-.updateproject(lu = .lastupdate)
+.updateproject(lu = .lastupdate,
+               loc = .localupdate)
