77f1a61057edf40da30eb47d5e2cb09003aec9b6
Vegard Lysne <vegard.lysne@helsedir.no>
2024-07-29 11:31:45 +0200

    update packages

--- R/updateproject.R
+++ R/updateproject.R
@@ -1,5 +1,5 @@
 # Change according to when the user files or packages were last updated
-.lastupdate <- "25.06.2024"
+.lastupdate <- "29.07.2024"
 
 # Find latest local update date
 .localupdate <- character()
